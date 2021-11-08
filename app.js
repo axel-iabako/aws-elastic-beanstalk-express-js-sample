387a7f6be800c781aaf5a1aaf01b0b7277647140
axel-iabako <axel@iabako.com>
2021-11-08 15:19:21 +0100

    V3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test CI AWS pour IABAKO v2!'));
+app.get('/', (req, res) => res.send('Test CI AWS pour IABAKO v3!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
